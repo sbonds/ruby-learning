b057d78af7d3afba8a3cc2798b8414d22102dd0c
Steve Bonds <sbonds@gmail.com>
2019-01-21 14:36:00 -0800

    Starting the Number Game in chapter 2

---
+++ head first ruby/02 Methods and Classes/0010 Number Game input.rb
@@ -0,0 +1,11 @@
+# Get my number game
+# From Head First Ruby
+# Practice by Steve Bonds
+# Jan 21 2019
+
+puts "Welcome to 'Get My Number!'"
+print "What is your name? "
+
+input = gets
+
+puts "Welcome, #{input}"
