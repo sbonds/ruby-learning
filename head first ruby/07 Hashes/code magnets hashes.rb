4532ab1943abd9ae38a4e793870692ec62e3f2b0
Steve Bonds <sbonds@gmail.com>
2019-01-22 15:39:49 -0800

    code magnets unsolved

---
+++ head first ruby/07 Hashes/code magnets hashes.rb
@@ -0,0 +1,19 @@
+volume
+result=
+options[:depth]
+width: 10,
+volume
+end
+options[:height]
+)
+*
+height: 5,
+def
+(options)
+options[:width]
+(
+*
+depth: 2.5
+puts "Volume is #{result}"
+
+# Output should be: "Volume is 125.0"
