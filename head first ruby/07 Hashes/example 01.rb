d17caa5d217bd2c55026b981f744c08aee54a726
Steve Bonds <sbonds@gmail.com>
2019-01-22 14:04:06 -0800

    example 1 solved

--- head first ruby/07 Hashes/example 01.rb
+++ head first ruby/07 Hashes/example 01.rb
@@ -1,8 +1,8 @@
-my_hash = {"one" => ______, :three => "four", __ => "six"}
+my_hash = {"one" => "two", :three => "four", 5 => "six"}
 puts my_hash[5]
 puts my_hash["one"]
-puts my_hash[______]
-my_hash[_______] = 8
+puts my_hash[:three]
+my_hash["seven"] = 8
 puts my_hash["seven"]
 
 =begin
