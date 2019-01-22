e8ac7c37b76a187042b4491428b807a1a06b87c5
Steve Bonds <sbonds@gmail.com>
2019-01-22 11:39:27 -0800

    Exercise 1-3 before solution

---
+++ head first ruby/06 Block return values/exercise 1-3.rb
@@ -0,0 +1,14 @@
+___________________
+
+File.open("sample.txt") do |file|
+    contents = file.readlines
+end
+
+puts contents
+
+=begin
+Expected output:
+This is the first line in the file.
+This is the second.
+This is the last line.
+=end
