bb239cb6b84c67e7e5fbca49712991f6d3a1ef6f
Steve Bonds <sbonds@gmail.com>
2019-01-22 15:46:32 -0800

    Exercise 3 unsolved

---
+++ head first ruby/07 Hashes/exercise 03.rb
@@ -0,0 +1,20 @@
+def create(options = {})
+  puts "Creating #{options[:database]} for owner #{options[:user]}..."
+end
+
+def connect(database:, host: "localhost", port: 3306, user: "root")
+  puts "Connecting to #{database} on #{host} port #{port} as #{user}..."
+end
+
+=begin
+
+What is the output for each of these:
+
+create(database: "catalog", user: "carl")
+create(user: "carl")
+create
+connect(database: "catalog")
+connect(database: "catalog", password: "1234")
+connect(user: "carl")
+
+=end
