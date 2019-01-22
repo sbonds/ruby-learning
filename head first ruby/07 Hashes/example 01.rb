c07df5ac69f1950d0cfa7311f4fd7ef877850c04
Steve Bonds <sbonds@gmail.com>
2019-01-22 14:02:31 -0800

    Chapter 7 example 1 unsolved

---
+++ head first ruby/07 Hashes/example 01.rb
@@ -0,0 +1,15 @@
+my_hash = {"one" => ______, :three => "four", __ => "six"}
+puts my_hash[5]
+puts my_hash["one"]
+puts my_hash[______]
+my_hash[_______] = 8
+puts my_hash["seven"]
+
+=begin
+Desired output
+
+six
+two
+four
+8
+=end
