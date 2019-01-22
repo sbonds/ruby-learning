cf857ab8537523e724fa9793484fa0d7dca0d5aa
Steve Bonds <sbonds@gmail.com>
2019-01-22 12:03:42 -0800

    Code magnets (scrambled)

---
+++ head first ruby/06 Block return values/code magnets.rb
@@ -0,0 +1,28 @@
+puts "Preheat oven to 375 degrees F"
+puts "Place #{ingredients} in dish"
+puts "Bake for 20 minutes"
+"rice, broccoli, and chicken"
+"noodles, celery, and tuna"
+def
+end
+=
+do
+end
+yield
+do
+end
+ingredients
+make_casserole
+make_casserole
+make_casserole
+
+=begin
+Expected output:
+
+Preheat oven to 375 degrees F
+Place noodles, celery, and tuna in dish
+Bake for 20 minutes
+Preheat oven to 375 degrees F
+Place rice, broccoli, and chicken in dish
+Bake for 20 minutes
+=end
