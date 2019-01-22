ebed7318bf811504314348097819f70794ba7c43
Steve Bonds <sbonds@gmail.com>
2019-01-22 09:17:13 -0800

    Exercise first try answers

---
+++ head first ruby/05 Arrays and blocks/exercise 1.rb
@@ -0,0 +1,25 @@
+def call_block(&block)
+  puts 1
+  block.call
+  puts 3
+end
+def call_twice
+  puts 1
+  yield
+  yield
+  puts 3
+end
+def pass_parameters_to_block
+  puts 1
+  yield 9, 3
+  puts 3
+end
+
+=begin
+B
+E
+A
+F
+D
+C
+=end
