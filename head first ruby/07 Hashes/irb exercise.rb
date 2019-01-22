aeeb0fe9a90ff4c77baa2fe56585144d415b8c14
Steve Bonds <sbonds@gmail.com>
2019-01-22 14:08:12 -0800

    Solution attempt

--- head first ruby/07 Hashes/irb exercise.rb
+++ head first ruby/07 Hashes/irb exercise.rb
@@ -1,10 +1,10 @@
 # IRB behaves poorly on some inputs, working with a file seems simpler
-protons = { "He" => 2 } 
-protons["He"]
-protons["C"] = 6
-protons["C"]
-protons.has_key?("C")
-protons.has_value?(119)
-protons.keys
-protons.values
-protons.merge({ "C" => 0, "Uh" => 147.2 })
+protons = { "He" => 2 } # { "He"=>2 }
+protons["He"] # 2
+protons["C"] = 6 # {"He"=>2,"C"=>6}
+protons["C"] # 6
+protons.has_key?("C") # true
+protons.has_value?(119) # false
+protons.keys # [ "He", "C"]
+protons.values # [ 2, 6 ]
+protons.merge({ "C" => 0, "Uh" => 147.2 }) # { "He"=>2,"C"=>0,"Uh"=>147.2}
