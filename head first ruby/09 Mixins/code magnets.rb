25268c3a8ca92824bab1965b80de5d31173be7db
Steve Bonds <sbonds@gmail.com>
2019-01-23 11:38:21 -0800

    Mixins code magnets unsolved

---
+++ head first ruby/09 Mixins/code magnets.rb
@@ -0,0 +1,32 @@
+Monkey
+Curious
+Clumsy
+Curious
+Clumsy
+end
+puts "Looks at #{thing}"
+end
+end
+puts "Knocks over #{thing}"
+end
+end
+bubbles =
+Monkey.new
+module
+module
+class
+def investigate(thing)
+investigate("vase")
+def break(thing)
+break("vase")
+include
+include
+include
+bubbles.
+bubbles.
+
+=begin
+Output:
+Looks at vase
+Knocks over vase
+=end
