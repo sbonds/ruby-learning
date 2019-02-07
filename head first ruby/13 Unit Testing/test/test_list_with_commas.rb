03ee794dc13cd64b6d1da7b95e82db7e835107fe
Steve Bonds <sbonds@gmail.com>
2019-02-07 11:45:17 -0800

    Use assert_equal to provide useful data

--- head first ruby/13 Unit Testing/test/test_list_with_commas.rb
+++ head first ruby/13 Unit Testing/test/test_list_with_commas.rb
@@ -6,18 +6,18 @@ class TestListWithCommas < Minitest::Test
     def test_it_joins_two_words_with_and
         list = ListWithCommas.new
         list.items = [ 'apple', 'orange']
-        assert('apple and orange' == list.join)
+        assert_equal('apple and orange', list.join)
     end
 
     def test_it_joins_three_words_with_commas
         list = ListWithCommas.new
         list.items = [ 'apple', 'orange', 'pear']
-        assert('apple, orange, and pear' == list.join)
+        assert_equal('apple, orange, and pear', list.join)
     end
 
     def test_it_prints_one_word_alone
         list = ListWithCommas.new
         list.items = [ 'apple' ]
-        assert('apple' == list.join)
+        assert_equal('apple', list.join)
     end
 end
