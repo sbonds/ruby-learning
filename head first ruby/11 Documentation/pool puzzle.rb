f5bbf01a46587d1451eed5b7e9e7f4fa6a3a9c31
Steve Bonds <sbonds@gmail.com>
2019-02-06 17:10:49 -0800

    Pool puzzle (unsolved)

---
+++ head first ruby/11 Documentation/pool puzzle.rb
@@ -0,0 +1,30 @@
+array = [10, 5, 7, 3, 9]
+
+first = array._______
+puts "We pulled #{first} off the start of the array."
+
+last = array._______
+puts "We pulled #{last} off the end of the array."
+
+largest = array._______
+puts "The largest remaining number is #{largest}."
+
+=begin
+Output goal:
+
+We pulled 10 off the start of the array.
+We pulled 9 off the end of the array.
+The largest remaining number is 7.
+
+Pool:
+shift
+clear
+lazy
+max
+uniq
+cycle
+reverse
+shuffle
+pop
+
+=end
