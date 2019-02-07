4941189e621c5eb3da4e41e3d0411e5e0ff378b7
Steve Bonds <sbonds@gmail.com>
2019-02-07 11:29:15 -0800

    Unit testing exercise solved

--- head first ruby/13 Unit Testing/exercise.rb
+++ head first ruby/13 Unit Testing/exercise.rb
@@ -1,10 +1,10 @@
-require '____/autorun'
+require 'minitest/autorun'
 
-class TestMath < ______
-    def ---_truth
-        ____(2 + 2 == 4)
+class TestMath < MiniTest::Test
+    def test_truth
+        assert(2 + 2 == 4)
     end
-    def ---_fallacy
-        ___(2 + 2 == 5)
+    def test_fallacy
+        assert(2 + 2 == 5)
     end
 end
