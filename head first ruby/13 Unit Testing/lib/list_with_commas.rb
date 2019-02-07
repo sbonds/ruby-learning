07451c6990f152969438f2635051abefbf342afe
Steve Bonds <sbonds@gmail.com>
2019-02-07 11:47:08 -0800

    Change code to make test succeed

--- head first ruby/13 Unit Testing/lib/list_with_commas.rb
+++ head first ruby/13 Unit Testing/lib/list_with_commas.rb
@@ -1,7 +1,9 @@
 class ListWithCommas
     attr_accessor :items
     def join
-        if items.length == 2
+        if items.length == 1
+            return items[0]
+        elsif items.length == 2
             return "#{items[0]} and #{items[1]}"
         end
         last_item = "and #{items.last}"
