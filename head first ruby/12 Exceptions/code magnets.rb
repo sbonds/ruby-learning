73de11fac89bde1b0ee5959b2523d3e5dbec7c9b
Steve Bonds <sbonds@gmail.com>
2019-02-07 05:30:35 -0800

    Chapter 12 code magnets (unsolved)

---
+++ head first ruby/12 Exceptions/code magnets.rb
@@ -0,0 +1,20 @@
+=begin
+def
+end
+end
+begin
+if destination == "Hawaii"
+(destination)
+drive
+end
+"You can't drive to Hawaii!"
+puts error.message
+=>
+rescue
+drive("Hawaii")
+error
+raise
+
+Output expected:
+You can't drive to Hawaii!
+=end
