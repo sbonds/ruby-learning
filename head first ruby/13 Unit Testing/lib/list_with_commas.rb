fca824d8e0e87d7b2282c879d6e80b036495ff2b
Steve Bonds <sbonds@gmail.com>
2019-02-07 11:41:25 -0800

    Add special no-comma case for two items

--- head first ruby/13 Unit Testing/lib/list_with_commas.rb
+++ head first ruby/13 Unit Testing/lib/list_with_commas.rb
@@ -1,6 +1,9 @@
 class ListWithCommas
     attr_accessor :items
     def join
+        if items.length == 2
+            return "#{items[0]} and #{items[1]}"
+        end
         last_item = "and #{items.last}"
         other_items = items.slice(0,items.length - 1).join(', ')
         "#{other_items}, #{last_item}"
