0c951bdea8d552742cdf465ba870edfe2b9f0fb8
Steve Bonds <sbonds@gmail.com>
2019-02-07 14:40:03 -0800

    Chapter 15 code magnets (unsolved)

---
+++ head first ruby/15 Saving and loading/code magnets.rb
@@ -0,0 +1,30 @@
+=begin
+Code magnets:
+
+"Head First Ruby"
+end
+book.pages =
+store
+store.transaction
+end
+do
+450
+YAML::Store.new
+book.title =
+book = Book.new
+book
+'yaml/store'
+store = 
+class Book
+attr_accessor :title, :pages
+require
+["HFRB"]
+=
+('books.yaml')
+
+Expected output:
+---
+HFRB: !ruby/object:Book
+  title: Head First Ruby
+  pages: 450
+=end
