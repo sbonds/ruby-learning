1e068c64955f46c83cb2652a5d4719806a614770
Steve Bonds <sbonds@gmail.com>
2019-02-08 09:39:15 -0800

    What a spec might look like

--- effective testing with rspec3/01-getting-started/sandwich_spec.rb
+++ effective testing with rspec3/01-getting-started/sandwich_spec.rb
@@ -1,4 +1,10 @@
 RSpec.describe 'An ideal sandwich' do
     it 'is delicious' do
+        sandwich = Sandwich.new('delicous',[])
+
+        taste = sandwich.taste
+
+        expect(taste).to eq('delicious')
+
     end
 end
