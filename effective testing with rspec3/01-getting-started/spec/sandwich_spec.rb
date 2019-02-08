25be46561d62fcf40074e89dc8b029d30164e195
Steve Bonds <sbonds@gmail.com>
2019-02-08 12:08:47 -0800

    Rework to not use instance variable
    This way the existing use of variable don't need to add
    @ signs

--- effective testing with rspec3/01-getting-started/spec/sandwich_spec.rb
+++ effective testing with rspec3/01-getting-started/spec/sandwich_spec.rb
@@ -1,9 +1,12 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
-    it 'is delicious' do
-        sandwich = Sandwich.new('delicious',[])
 
+    def sandwich
+        Sandwich.new('delicious',[])
+    end
+
+    it 'is delicious' do
         taste = sandwich.taste
 
         expect(taste).to eq('delicious')
@@ -11,7 +14,6 @@ RSpec.describe 'An ideal sandwich' do
     end
 
     it 'lets me add toppings' do
-        sandwich = Sandwich.new('delicious', [])
         sandwich.toppings << 'cheese'
         toppings = sandwich.toppings
         expect(toppings).not_to be_empty
