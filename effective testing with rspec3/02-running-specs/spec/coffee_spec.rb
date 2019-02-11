dbfc53ed062181f38e7f76c720490b23403df64a
Steve Bonds <sbonds@gmail.com>
2019-02-11 14:52:05 -0800

    Nice way to do test-driven development.

--- effective testing with rspec3/02-running-specs/spec/coffee_spec.rb
+++ effective testing with rspec3/02-running-specs/spec/coffee_spec.rb
@@ -46,8 +46,15 @@ RSpec.describe 'A cup of coffee' do
             expect(coffee.price).to eq(1.25)
         end
         
-        it 'is light in color'
+        it 'is light in color' do
+            pending 'Color not implemented yet'
+            expect(coffee.color).to be(:light)
+        end
 
-        it 'is cooler than 200 degrees Fahrenheit'
+        it 'is cooler than 200 degrees Fahrenheit' do
+            pending 'Temperature not implemented yet'
+            expect(coffee.temperature).to be < 200.0
+        end
+
     end
 end
