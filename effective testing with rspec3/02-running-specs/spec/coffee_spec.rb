811705aeccf1ea92e468512414599b623055056a
Steve Bonds <sbonds@gmail.com>
2019-02-11 14:55:35 -0800

    Unexpected success counts as a failure.

--- effective testing with rspec3/02-running-specs/spec/coffee_spec.rb
+++ effective testing with rspec3/02-running-specs/spec/coffee_spec.rb
@@ -56,12 +56,10 @@ RSpec.describe 'A cup of coffee' do
         end
         
         it 'is light in color' do
-            pending 'Color not implemented yet'
             expect(coffee.color).to be(:light)
         end
 
         it 'is cooler than 200 degrees Fahrenheit' do
-            pending 'Temperature not implemented yet'
             expect(coffee.temperature).to be < 200.0
         end
 
