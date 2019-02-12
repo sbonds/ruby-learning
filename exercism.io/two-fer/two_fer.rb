598452433f05117229784ff11ccd50ff9735f553
Steve Bonds <sbonds@gmail.com>
2019-02-12 15:47:38 -0800

    Define two_fer based on hints in error message.

--- exercism.io/two-fer/two_fer.rb
+++ exercism.io/two-fer/two_fer.rb
@@ -25,25 +25,34 @@ Skipped, no message given
   3) Skipped:
 TwoFerTest#test_a_name_given [two-fer/two_fer_test.rb:12]:
 Skipped, no message given
+
+This line:
+ NameError: uninitialized constant TwoFerTest::TwoFer
+Suggests we need a class "TwoFerTest" with a method TwoFer
+
+New error:
+ two-fer/two_fer_test.rb:5:in `<main>': superclass mismatch for class TwoFerTest (TypeError)
+
+Based on Google search
+ https://stackoverflow.com/questions/5512023/ruby-on-rails-3-superclass-mismatch-for-class
+This means we have a duplicate class. Perhaps taking "Test" off the end is what we want.
+Seems a bit vague though...
+
+New error:
+
+  1) Error:
+TwoFerTest#test_no_name_given:
+NoMethodError: undefined method `two_fer' for TwoFer:Class
+    two-fer/two_fer_test.rb:8:in `test_no_name_given'
+
+That implies we're looking to define that undefined method. Consider it done!
+
 =end
-
-# This line:
-#  NameError: uninitialized constant TwoFerTest::TwoFer
-# Suggests we need a class "TwoFerTest" with a method TwoFer
-
-# New error:
-#  two-fer/two_fer_test.rb:5:in `<main>': superclass mismatch for class TwoFerTest (TypeError)
-
-# Based on Google search
-#  https://stackoverflow.com/questions/5512023/ruby-on-rails-3-superclass-mismatch-for-class
-# This means we have a duplicate class. Perhaps taking "Test" off the end is what we want.
-# Seems a bit vague though...
-
 
 # TODO: Ask if there's a better way to determine the desired class name from the
 # test failure output.
 
 class TwoFer
-  def TwoFer
+  def two_fer
   end
 end
