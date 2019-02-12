f8bec7d1b166f4fe141e2435d53f39e4ad09f90d
Steve Bonds <sbonds@gmail.com>
2019-02-12 15:27:23 -0800

    Perhaps the function needs a specific name?

--- exercism.io/hello-world/hello_world.rb
+++ exercism.io/hello-world/hello_world.rb
@@ -1,5 +1,15 @@
 # Read the instructions better:
 #   Write a function that returns the string "Hello, World!".
-def greet_the_planet
+=begin
+Test output:
+  1) Error:
+HelloWorldTest#test_say_hi:
+NameError: uninitialized constant HelloWorldTest::HelloWorld
+Did you mean?  HelloWorldTest
+
+Perhaps it's not just any function name, but a specific function name
+expected. Like "HelloWorld"?
+=end
+def HelloWorld
   "Hello, World!"
 end
